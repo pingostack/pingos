5eb8f96414012ed27546a7dd4245ba23a2f573a5
im-pingo <cczjp89@gmail.com>
2019-11-28 21:09:25 +0800

    fix release.sh : check the version of linux

--- release.sh
+++ release.sh
@@ -91,24 +91,38 @@ scanOS(){
     OS=`uname -s`
     if [ ${OS} == "Darwin" ]
     then
-        echo "brew"
+        PM="brew"
     elif [ ${OS} == "Linux" ]
     then
-        source /etc/os-release
-        case $ID in
-            debian|ubuntu|devuan)
-                echo "apt-get"
-                ;;
-            centos|fedora|rhel)
-                echo "yum"
-                ;;
-            *)
-                exit 1
-                ;;
-        esac
+        if grep -Eqi "CentOS" /etc/issue || grep -Eq "CentOS" /etc/*-release; then
+            DISTRO='CentOS'
+            PM='yum'
+        elif grep -Eqi "Red Hat Enterprise Linux Server" /etc/issue || grep -Eq "Red Hat Enterprise Linux Server" /etc/*-release; then
+            DISTRO='RHEL'
+            PM='yum'
+        elif grep -Eqi "Aliyun" /etc/issue || grep -Eq "Aliyun" /etc/*-release; then
+            DISTRO='Aliyun'
+            PM='yum'
+        elif grep -Eqi "Fedora" /etc/issue || grep -Eq "Fedora" /etc/*-release; then
+            DISTRO='Fedora'
+            PM='yum'
+        elif grep -Eqi "Debian" /etc/issue || grep -Eq "Debian" /etc/*-release; then
+            DISTRO='Debian'
+            PM='apt-get'
+        elif grep -Eqi "Ubuntu" /etc/issue || grep -Eq "Ubuntu" /etc/*-release; then
+            DISTRO='Ubuntu'
+            PM='apt-get'
+        elif grep -Eqi "Raspbian" /etc/issue || grep -Eq "Raspbian" /etc/*-release; then
+            DISTRO='Raspbian'
+            PM='apt-get'
+        else
+            DISTRO='unknow'
+        fi
     else
         echo ${OS}
     fi
+
+    echo ${PM}
 }
 
 modules=(
